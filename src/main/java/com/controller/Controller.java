9804b1f25d69acdf6a9f58e865023d6c91bfa251
Zheng <zheng@Surongs-MacBook-Pro.local>
2016-05-09 18:03:00 -0700

    second

--- src/main/java/com/controller/Controller.java
+++ src/main/java/com/controller/Controller.java
@@ -3,101 +3,78 @@ import com.content.*;
 import com.model.StudentRepository;
 import com.model.TimeTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+
 @RestController
 public class Controller {
-	
+
 	@Autowired
 	private TimeTableRepository repository;
 	@Autowired
 	private StudentRepository studentrepository;
 
-	@RequestMapping(value="/signout", method=RequestMethod.POST)
-	public ResponseEntity<String> logout(@CookieValue(value="sid",defaultValue="888888") String sid,HttpServletRequest request) {
-		System.out.println("sign out sid: "+sid);
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Set-Cookie","sid="+sid+"; max-age=0");
-		return new ResponseEntity<>("Clean Cookie", headers,HttpStatus.OK);
-		/*	for(Cookie cookie : request.getCookies()) {
-		if(cookie.getName() == "sid") {
-				//Clear one cookie
-				cookie.setName("");
-				cookie.setMaxAge(0);
-				cookie.setPath(request.getContextPath());
-				response.addCookie(cookie);
-				//Clear the other cookie
-				Cookie cookieWithSlash = cookie.clone();
-				cookieWithSlash.setPath(request.getContextPath() + "/");
-				response.addCookie(cookieWithSlash);
-			}
-		}*/
-	}
-
 
 	@RequestMapping(value="/studentall", method=RequestMethod.GET)
 	public ResponseEntity<List<Student>> getAllStudent() {
 		List<Student> listStudent = studentrepository.findAll();
-    	if (listStudent == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(listStudent, HttpStatus.OK);
+		if (listStudent == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(listStudent, HttpStatus.OK);
 	}
 
 	@RequestMapping(value="/admin", method=RequestMethod.POST)
 	public ResponseEntity<Long> setMaxNum(@RequestParam("maxNum") int maxNum ) {
 		List<Student> listStudent = studentrepository.findAll();
-    	if (listStudent == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    	for(int i = 0; i < listStudent.size(); i++) {
-    		if(!listStudent.get(i).getStudentName().equals("admin")) {
-    			listStudent.get(i).setMaxNum(maxNum);
-        		listStudent.get(i).setRole("1");
-    		} else {
-    			listStudent.get(i).setMaxNum(Integer.MAX_VALUE);
-        		listStudent.get(i).setRole("0");
-    		}
-    	}
-    	studentrepository.save(listStudent);
-        return new ResponseEntity<>(HttpStatus.OK);
-	}	
-	
+		if (listStudent == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		for(int i = 0; i < listStudent.size(); i++) {
+			if(!listStudent.get(i).getStudentName().equals("admin")) {
+				listStudent.get(i).setMaxNum(maxNum);
+				listStudent.get(i).setRole("1");
+			} else {
+				listStudent.get(i).setMaxNum(Integer.MAX_VALUE);
+				listStudent.get(i).setRole("0");
+			}
+		}
+		studentrepository.save(listStudent);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@RequestMapping(value="/admin", method=RequestMethod.GET)
+	public ResponseEntity<Integer> getMaxNumer() {
+		int num= getMaxNum();
+		return new ResponseEntity<>(num,HttpStatus.OK);
+	}
+
+
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public ResponseEntity<Student> getStudentById(@RequestParam(value = "id", required = true) String id,
-												@RequestParam(value = "password", required = true) String password,HttpServletResponse response) {
+												  @RequestParam(value = "password", required = true) String password) {
 		Student s = studentrepository.findById(id);
-    	if (s == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    	if(s.getPassword().equals(password)) {
-    		//HttpHeaders headers = new HttpHeaders();
-        	//headers.add("Set-Cookie","sid="+id+"; path=/;max-age=3600;");
-			Cookie c_id = new Cookie("sid",id); //
-			c_id.setMaxAge(1000); //set expire time to 1000 sec
-
-			response.addCookie(c_id); //put cookie in response
+		if (s == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		if(s.getPassword().equals(password)) {
+			//HttpHeaders headers = new HttpHeaders();
+			//headers.add("Set-Cookie","sid="+id+"; path=/; max-age=3600;");
 			return new ResponseEntity<>(s, HttpStatus.OK);
-
-			//return new ResponseEntity<>(s,headers, HttpStatus.OK);
-    	}
-        return new ResponseEntity<>(s, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(s, HttpStatus.NOT_FOUND);
 	}
 
 	@RequestMapping(value="/student", method=RequestMethod.GET)
-	public ResponseEntity<Student> findStudentById(@CookieValue(value="sid", defaultValue="123456") String sid) {
+	public ResponseEntity<Student> findStudentById(@RequestParam("sid") String sid) {
 		Student s = studentrepository.findById(sid);
 
 		if(s == null) {
@@ -106,25 +83,29 @@ public class Controller {
 		return new ResponseEntity<>(s, HttpStatus.OK);
 
 	}
+
 	@RequestMapping(value="/student/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Long> delStudentById(@PathVariable("id") String id ) {
 		Long l = studentrepository.deleteById(id);
-    	if (l == 0) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(l, HttpStatus.OK);
-	}	
-	
-	@RequestMapping(value="/registration", method=RequestMethod.POST)
+		if (l == 0) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(l, HttpStatus.OK);
+	}
+
+	@RequestMapping(value="/register", method=RequestMethod.POST)
 	public ResponseEntity<Student> createStudentInfo(@RequestBody Student input) {
+		int max=getMaxNum();
+
 		Student s = studentrepository.findById(input.getId());
 		if(s != null) {
-			s.setPassword(input.getPassword());
+			return new ResponseEntity<>(s, HttpStatus.CONFLICT);
+			/*s.setPassword(input.getPassword());
 			s.setStudentName(input.getStudentName());
-			s.setMaxNum(input.getMaxNum());
-			studentrepository.save(s);
+			s.setMaxNum(max);
+			studentrepository.save(s);*/
 		} else {
-			s = new Student(input.getId(), 0, input.getMaxNum(), input.getStudentName(), input.getPassword());
+			s = new Student(input.getId(), 0, max, input.getStudentName(), input.getPassword());
 			if(input.getStudentName().equals("admin")) {
 				s.setRole("0");
 				s.setMaxNum(Integer.MAX_VALUE);
@@ -133,45 +114,64 @@ public class Controller {
 			}
 			studentrepository.save(s);
 		}
-    	
-        return new ResponseEntity<>(HttpStatus.OK);
+
+		return new ResponseEntity<>(s, HttpStatus.OK);
 	}
-	
+
+
+
+
+	private int getMaxNum() {
+
+
+		List<Student> listStudent = studentrepository.findAll();
+		if (listStudent == null) {
+			return 3;
+		}
+		for(int i = 0; i < listStudent.size(); i++) {
+			if(!listStudent.get(i).getStudentName().equals("admin")) {
+				return listStudent.get(i).getMaxNum();
+			}
+		}
+		return 3;
+	}
+
+
+
 	@RequestMapping(value="/timetable/{date}", method=RequestMethod.GET)
 	public ResponseEntity<TimeTable> getResvInfoByDate(@PathVariable("date") String date) {
 		TimeTable tt = repository.findByDate(date);
-    	if (tt == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(tt, HttpStatus.OK);
+		if (tt == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(tt, HttpStatus.OK);
 	}
 
 	@RequestMapping(value="/timetable", method=RequestMethod.GET)
 	public ResponseEntity<List<TimeTable>> getAllResvInfo() {
 		List<TimeTable> tt = repository.findAll();
-    	if (tt == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(tt, HttpStatus.OK);
+		if (tt == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(tt, HttpStatus.OK);
 	}
 
-	
+
 	@RequestMapping(value="/book", method=RequestMethod.POST)
-	public ResponseEntity<TimeTable> resvRoom(@CookieValue(value="sid", defaultValue="888888") String sid, @RequestBody StudentResv input) {
+	public ResponseEntity<TimeTable> resvRoom(@RequestParam("sid") String sid, @RequestBody StudentResv input) {
 		//check student status
-		System.out.println("book sid: " + sid);
 		Student s = studentrepository.findById(sid);
 		if(s == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-		Date today = new Date();        
+		Date today = new Date();
 		String curDate = df.format(today);
 		Calendar cal = Calendar.getInstance();
 		int curSlot = cal.get(Calendar.HOUR_OF_DAY);
-		
-		if(input.getDate().compareTo(curDate) < 0 
-		|| (input.getDate().compareTo(curDate) == 0 && (input.getSlot()+8) <= curSlot)) {
+
+		if(input.getDate().compareTo(curDate) < 0
+				|| (input.getDate().compareTo(curDate) == 0 && (input.getSlot()+8) <= curSlot)) {
 			return new ResponseEntity<>(HttpStatus.NON_AUTHORITATIVE_INFORMATION);
 		}
 
@@ -192,7 +192,7 @@ public class Controller {
 		}
 		s.getReservation().add(input);
 		s.setCurNum(s.getCurNum()+1);
-		
+
 		//check room status
 		TimeTable tt = repository.findByDate(input.getDate());
 		if (tt == null) {
@@ -200,7 +200,7 @@ public class Controller {
 			repository.save(tt);
 			studentrepository.save(s);
 			return new ResponseEntity<>(HttpStatus.CREATED);
-        }
+		}
 		for(ResvInfo r : tt.getTimesheet()) {
 			if(r.getRoomNumber().equals(input.getRoomid())) {
 				for(TimeSlot ts : r.getTimeSlots()) {
@@ -219,30 +219,29 @@ public class Controller {
 		studentrepository.save(s);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
-	
+
+	//	@RequestMapping(value="/book", method=RequestMethod.DELETE)
+//	public ResponseEntity<String> cancleRoom(@CookieValue("sid") String sid, @RequestBody StudentResv input) {
+//		return cancelRoom(sid, input);
+//	}
+//
 	@RequestMapping(value="/book", method=RequestMethod.DELETE)
-	public ResponseEntity<String> cancleRoom(@CookieValue(value="sid", defaultValue="123456") String sid, @RequestBody StudentResv input) {
-		System.out.println("cancel book sid: "+sid);
-		return cancelRoom(sid, input);
-	}
-	
-	@RequestMapping(value="/admin", method=RequestMethod.DELETE)
 	public ResponseEntity<String> cancleRoomByAdmin(@RequestParam("sid") String sid, @RequestBody StudentResv input) {
 		return cancelRoom(sid, input);
 	}
-	
+
 	private ResponseEntity<String> cancelRoom(String sid, StudentResv input) {
 		//check student status
 		Student s = studentrepository.findById(sid);
 		if(s == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		
+
 		for(int i = 0; i < s.getReservation().size(); i++) {
 			StudentResv studentresv = s.getReservation().get(i);
 			if(studentresv.getDate().equals(input.getDate())
-			&& studentresv.getRoomid().equals(input.getRoomid())
-			&& studentresv.getSlot() == input.getSlot()) {
+					&& studentresv.getRoomid().equals(input.getRoomid())
+					&& studentresv.getSlot() == input.getSlot()) {
 				s.getReservation().remove(i);
 				s.setCurNum(s.getCurNum()-1);
 				break;
@@ -252,9 +251,10 @@ public class Controller {
 		TimeTable tt = repository.findByDate(input.getDate());
 		if (tt == null) {
 			return new ResponseEntity<>(HttpStatus.CREATED);
-        }
+		}
 
-		for(ResvInfo r : tt.getTimesheet()) {
+		for(int n = 0; n < tt.getTimesheet().size(); n++) {
+			ResvInfo r = tt.getTimesheet().get(n);
 			if(r.getRoomNumber().equals(input.getRoomid())) {
 				for(int i = 0; i <  r.getTimeSlots().size(); i++) {
 					if(r.getTimeSlots().get(i).getTimeSlot() == input.getSlot()) {
@@ -262,9 +262,15 @@ public class Controller {
 					}
 				}
 			}
+			if(r.getTimeSlots().size() == 0) {
+				tt.getTimesheet().remove(n);
+			}
 		}
-		
-		repository.save(tt);
+		if(tt.getTimesheet().size() == 0) {
+			repository.delete(tt);
+		} else {
+			repository.save(tt);
+		}
 		studentrepository.save(s);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
